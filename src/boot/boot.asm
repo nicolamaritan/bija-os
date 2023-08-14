4303b81fd127e301638970369a6a614ef65d1bcf
Nicola Maritan <nicolamaritan01@gmail.com>
2023-08-14 23:42:12 +0200

    Kernel loaded from second sector. Added ATA read driver.

--- src/boot/boot.asm
+++ src/boot/boot.asm
@@ -28,8 +28,7 @@ step2:
     mov eax, cr0
     or eax, 0x1
     mov cr0, eax
-    ;jmp CODE_ENTRY_OFFSET:load32
-    jmp $
+    jmp CODE_ENTRY_OFFSET:load32    ; Load CS with CODE_ENTRY_OFFSET (0x08) and jumps to load32
 
 ; GDT
 ; Paging will be used instead of Segmentation in Protected Mode.
@@ -63,6 +62,75 @@ gdt_descriptor:
     dw gdt_end - gdt_start-1
     dd gdt_start
  
+
+[BITS 32]
+load32:
+    mov eax, 1
+    mov ecx, 100
+    mov edi, 0x100000
+    call ata_lba_read
+    jmp CODE_ENTRY_OFFSET:0x100000
+
+;=============================================================================
+; ATA read sectors (LBA mode) 
+;
+; @param EAX Logical Block Address of sector
+; @param ECX  Number of sectors to read
+; @param EDI The address of buffer to put data obtained from disk
+;
+; @return None
+;=============================================================================
+ata_lba_read:
+    mov ebx, eax ; Backup the LBA
+
+    ; Send the lowest 8 bits of the LBA to the HD controller
+    shr eax, 24     ; Get bit 24 - 27 in al
+    or eax, 0xE0    ; Select Master Drive
+    mov dx, 0x1F6   ; Port to send drive and bit 24 - 27 of LBA
+    out dx, al
+
+    ; Send the number of sectors to read
+    mov eax, ecx    ; Get number of sectors from ECX
+    mov dx, 0x1F2   ; Port to send number of sectors
+    out dx, al
+
+    mov eax, ebx    ; Restore LBA backup
+    mov dx, 0x1F3   ; Port to send bit 0 - 7 of LBA
+    out dx, al
+
+    mov dx, 0x1F4   ; Port to send bit 8 - 15 of LBA
+    mov eax, ebx
+    shr eax, 8
+    out dx, al
+
+    mov dx, 0x1F5   ; Port to send bit 16 - 23 of LBA
+    mov eax, ebx
+    shr eax, 16
+    out dx, al
+
+    mov dx, 0x1F7
+    mov al, 0x20    ; Read with retry
+    out dx, al
+
+    ; Read all sectors into memory
+.next_sector:
+    push ecx
+
+; Checking if we need to read
+.try_again:
+    mov dx, 0x1F7
+    in al, dx
+    test al, 8      ; Checks if the disk can service
+    jz .try_again   ; Jump if the test fails
+
+    mov ecx, 256
+    mov dx, 0x1F0
+    rep insw        ; Repeat exc (256) times: reads 16 bits from the port 0x1F0 and stores it in ES:DI (0x00:0x100000)
+
+    pop ecx
+    loop .next_sector
+
+    ret
 
 
 
