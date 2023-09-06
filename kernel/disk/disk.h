25e72a41c49e0dc00220577d0b8757b4294b328f
Nicola Maritan <nicolamaritan01@gmail.com>
2023-09-06 15:15:42 +0200

    Added documentation.

--- kernel/disk/disk.h
+++ kernel/disk/disk.h
@@ -33,6 +33,25 @@ struct disk
  */
 int disk_read_block(struct disk* disk, size_t lba, size_t n, void* buffer);
 
+/**
+ * Writes one or more sectors to a disk using the Logical Block Address (LBA) method.
+ *
+ * This function sends commands to a disk controller to write specified sectors to a disk.
+ * The data to be written must be provided through a buffer.
+ *
+ * @param disk The logical disk to read from
+ * @param lba The starting Logical Block Address (LBA) of the sectors to be read.
+ * @param n   The number of sectors to read.
+ * @param buffer A pointer to the buffer where the read data will be stored.
+ *
+ * @return 0 if the read operation is successful, or an error code if it fails.
+ *
+ * @note The function assumes that the disk controller is set up correctly and that the
+ *       necessary hardware I/O ports are properly configured.
+ * @note The function uses the ATA/ATAPI command set for disk operations.
+ * @note It is the caller's responsibility to allocate and manage the memory for the buffer.
+ * @note This function may block until the disk is ready for reading.
+ */
 int disk_write_block(struct disk* disk, size_t lba, size_t n, void* buffer);
 
 /**
